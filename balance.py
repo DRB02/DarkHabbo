6a12f5a06d20fc1954b247b6a3e3f22ee1a6fa6a
Dylan <blokdylan@hotmail.com>
2023-08-19 15:44:45 +0200

    Update balance.py

--- balance.py
+++ balance.py
@@ -1,5 +1,17 @@
 #Gets a user unminted NFTc balance
 address = message.content.split(' ',1)[1]
-r = requests.get('https://nft.habbo.com/api/tokens/balance/?address='+address).json()
-balance = r['balance']
-await message.channel.send("Current balance: " + (str(balance)))
+if ".eth" in address:
+    r = requests.get('https://ensdata.net/'+address).json()
+    try:
+        address = r['address']
+        r = requests.get('https://nft.habbo.com/api/tokens/balance/?address='+address).json()
+        balance = r['balance']
+        await message.channel.send("Current balance: " + (str(balance)))
+    except:
+        await message.channel.send("You tried an invalid ENS")
+
+else:    
+    r = requests.get(
+        'https://nft.habbo.com/api/tokens/balance/?address='+address).json()
+    balance = r['balance']
+    await message.channel.send("Current balance: " + (str(balance)))
